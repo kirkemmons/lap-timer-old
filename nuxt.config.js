810c6858121e80118993d4f15f7be5b837c98c5f
kirkmonz <kirkemmons2@gmail.com>
2023-01-09 16:55:55 -0500

    add dayjs durations plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -106,7 +106,8 @@ export default {
     plugins: [
       'utc',
       'relativeTime',
-      'timezone'
+      'timezone',
+      'duration'
     ]
   },
 
--- pages/index.vue
+++ pages/index.vue
@@ -75,6 +75,9 @@
 
 import dayjs from 'dayjs'
 
+const duration = require('dayjs/plugin/duration')
+dayjs.extend(duration)
+
 export default {
 
   data () {
